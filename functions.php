f3cec999eab92167922ed41088379cc5b93f1363
Michael Edwards <smededwards@gmail.com>
2025-03-27 13:30:51 +0000

    feat: theme setup enhancements and block patterns removal
    - add theme setup function
    - add editor styles
    - remove core block patterns

--- functions.php
+++ functions.php
@@ -5,3 +5,38 @@
  * @package Menos
  * @version 0.1.0
  */
+
+ /**
+ * Add theme supports and styles.
+ *
+ * @since 0.1.0
+ */
+function menos_setup() {
+
+    // Enqueue editor styles.
+    add_editor_style( 'style.css' );
+
+	// Remove core block patterns.
+	remove_theme_support( 'core-block-patterns' );
+}
+add_action( 'after_setup_theme', 'menos_setup' );
+
+/**
+ * Enqueue styles.
+ *
+ * @since 0.1.0
+ */
+function menos_styles() {
+
+	// Register theme stylesheet.
+	wp_register_style(
+		'menos-styles',
+		get_template_directory_uri() . '/style.css',
+		array(),
+		wp_get_theme()->get( 'Version' ),
+	);
+
+	// Enqueue theme stylesheet.
+	wp_enqueue_style( 'menos-styles' );
+}
+add_action( 'wp_enqueue_scripts', 'menos_styles' );
